a004b986977f488c7488d641fb3ac36a5a39d384
hadyelhossiny <hady.elhossiny1132@gmail.com>
2020-09-04 22:32:27 +0200

    make it better

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 app.use(compression());
-app.use(helmet());
+//app.use(helmet());
 app.use(morgan('combined'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
