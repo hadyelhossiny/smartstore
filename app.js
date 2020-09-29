9f815f843f870d2e652ccf6fe8bc0e75eccb9a60
hadyelhossiny <hady.elhossiny1132@gmail.com>
2020-09-29 09:57:30 +0200

    make it better

--- app.js
+++ app.js
@@ -1,70 +1,71 @@
-const path = require('path');
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
-const compression = require('compression');
-const order = require('./models/order')
-const db = require('db')
-const multer = require('multer');
-var fs = require('fs');
-const Sensor = require('./models/sensor')
-const express = require('express');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
-const morgan = require ('morgan')
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const csrf = require('csurf');
-const flash = require('connect-flash');
+const compression = require("compression");
+const order = require("./models/order");
+const db = require("db");
+const multer = require("multer");
+var fs = require("fs");
+const Sensor = require("./models/sensor");
+const express = require("express");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const MONGODB_URI =
-  'mongodb+srv://smartstore:smartstore@cluster0.qzwse.mongodb.net/test';
+  "mongodb+srv://smartstore:smartstore@cluster0.qzwse.mongodb.net/test";
 
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: 'sessions'
+  collection: "sessions",
 });
 const csrfProtection = csrf();
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 //-------------------------------
-app.get('/todoss', function(req, res) {
-  order.find(function(err, orders) {
-      if (err) return console.error(err);
-      //console.log("existing todos are: ", todos);
-      // render index page, passing todos as local variable
-     // console.log(sensors[1])
-      res.send(orders);
-      //res.render('index', {
-        //  todos: todos
-     //});
+app.get("/todoss", function (req, res) {
+  order.find(function (err, orders) {
+    if (err) return console.error(err);
+    //console.log("existing todos are: ", todos);
+    // render index page, passing todos as local variable
+    // console.log(sensors[1])
+    res.send(orders);
+    //res.render('index', {
+    //  todos: todos
+    //});
   });
-})
-app.get('/display', function(req, res, next) {
-  return res.sendFile(path.join(__dirname + '/views/index.html'));
 });
-
+app.get("/display", function (req, res, next) {
+  return res.sendFile(path.join(__dirname + "/views/index.html"));
+});
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'images');
+    cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  }
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
   ) {
     cb(null, true);
   } else {
@@ -72,31 +73,31 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 app.use(compression());
 //app.use(helmet());
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(
   session({
-    secret: 'my secret',
+    secret: "my secret",
     resave: false,
     saveUninitialized: false,
-    store: store
+    store: store,
   })
 );
 app.use(csrfProtection);
@@ -107,11 +108,11 @@ app.use((req, res, next) => {
     return next();
   }
   User.findById(req.session.user._id)
-    .then(user => {
+    .then((user) => {
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
 app.use((req, res, next) => {
@@ -120,7 +121,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
 app.use(authRoutes);
@@ -128,59 +129,56 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 //------------------
-var mqtt = require('mqtt');
+var mqtt = require("mqtt");
 var options = {
-    port: 16577,
-    host: 'mqtt://hairdresser.cloudmqtt.com',
-    clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
-    username: 'kloyczpu',
-    password: '8rDBv6xYk4NU',
-    keepalive: 60,
-    reconnectPeriod: 1000,
-    protocolId: 'MQIsdp',
-    protocolVersion: 3,
-    clean: true,
-    encoding: 'utf8'
+  port: 16577,
+  host: "mqtt://hairdresser.cloudmqtt.com",
+  clientId: "mqttjs_" + Math.random().toString(16).substr(2, 8),
+  username: "kloyczpu",
+  password: "8rDBv6xYk4NU",
+  keepalive: 60,
+  reconnectPeriod: 1000,
+  protocolId: "MQIsdp",
+  protocolVersion: 3,
+  clean: true,
+  encoding: "utf8",
 };
 
-var client = mqtt.connect('mqtt:hairdresser.cloudmqtt.com', options);
-client.on('connect', function() { // When connected
-    console.log('connected');
-    //subscribe to a topic
-    client.subscribe('sensors/#', function() {
-        // when a message arrives, do something with it
-        client.on('message', function(topic, message, packet) {
-          var x = message.toString();
-          var y = JSON.parse(x)
-          console.log(y)
-          const sensor = new Sensor({
-            value : y
-          });
-          sensor
-            .save()
-            .then(result => {
-                // console.log(result);
-            })
-            .catch(err => {
-              console.log(err);
-           });
+var client = mqtt.connect("mqtt:hairdresser.cloudmqtt.com", options);
+client.on("connect", function () {
+  // When connected
+  console.log("connected");
+  //subscribe to a topic
+  client.subscribe("sensors/#", function () {
+    // when a message arrives, do something with it
+    client.on("message", function (topic, message, packet) {
+      var x = message.toString();
+      var y = JSON.parse(x);
+      console.log(y);
+      const sensor = new Sensor({
+        value: y,
+      });
+      sensor
+        .save()
+        .then((result) => {
+          // console.log(result);
+        })
+        .catch((err) => {
+          console.log(err);
         });
-  
     });
+  });
 
-    //publish a message to a topic
-    // setInterval(function(){client.publish('test', 'Hello mqtt')},1000)
+  //publish a message to a topic
+  // setInterval(function(){client.publish('test', 'Hello mqtt')},1000)
 });
 //-----------------
 
-
 mongoose
   .connect(MONGODB_URI)
-  .then(result => {
+  .then((result) => {
     app.listen(PORT);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
-
-
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,104 +1,119 @@
-const Product = require('../models/product');
-const Sensor = require('../models/sensor')
-const Order = require('../models/order');
-const user = require('../models/user');
-const nodemailer = require('nodemailer');
+const Product = require("../models/product");
+const Sensor = require("../models/sensor");
+const Order = require("../models/order");
+const user = require("../models/user");
+const nodemailer = require("nodemailer");
 const bodyparser = require("body-parser");
-const isAdmin = require('../middleware/is-admin');
-const fs = require('fs');
-const path = require('path');
-const ejs = require('ejs')
-
-
+const isAdmin = require("../middleware/is-admin");
+const fs = require("fs");
+const path = require("path");
+const ejs = require("ejs");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+const product = require("../models/product");
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key:
+        "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",
+    },
+  })
+);
 
 exports.getProducts = (req, res, next) => {
   Product.find()
-    .then(products => {
+    .then((products) => {
       console.log(products);
       products.sort();
       products.reverse();
-      res.render('shop/product-list', {
+      res.render("shop/product-list", {
         prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
+        pageTitle: "All Products",
+        path: "/products",
       });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
     });
 };
-
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
-    .then(product => {
-      
-      res.render('shop/product-detail', {
+    .then((product) => {
+      res.render("shop/product-detail", {
         product: product,
         pageTitle: product.title,
-        path: '/products'
+        path: "/products",
       });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.find()
-    .then(products => {
-      products.sort();
-      products.reverse();
-      res.render('shop/index', {
+  if (req.query.search) {
+    const regex = new RegExp(escapeRegex(req.query.search), "gi");
+    product.find({ title: regex }).then((products) => {
+      res.render("shop/index", {
         prods: products,
-        pageTitle: 'Shop',
-        path: '/'
+        pageTitle: "Shop",
+        path: "/",
       });
-    })
-    .catch(err => {
-      console.log(err);
     });
+  } else {
+    Product.find()
+      .then((products) => {
+        products.sort();
+        products.reverse();
+        res.render("shop/index", {
+          prods: products,
+          pageTitle: "Shop",
+          path: "/",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 };
 
 exports.getSensors = (req, res, next) => {
-  Sensor.find(function(err, sensors) {
+  Sensor.find(function (err, sensors) {
     if (err) return console.error(err);
     //console.log("existing todos are: ", todos);
     // render index page, passing todos as local variable
     var x = sensors.reverse();
     res.send(x);
     //res.render('index', {
-      //  todos: todos
-   //});
-
-    })
-    .catch(err => console.log(err));
+    //  todos: todos
+    //});
+  }).catch((err) => console.log(err));
 };
 exports.getCart = (req, res, next) => {
   req.user
-    .populate('cart.items.product')
+    .populate("cart.items.product")
     .execPopulate()
-    .then(user => {
+    .then((user) => {
       const products = user.cart.items;
       // products.sort();
       products.reverse();
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: products
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: products,
       });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
-    .then(product => {
+    .then((product) => {
       return req.user.addToCart(product);
     })
-    .then(result => {
+    .then((result) => {
       console.log(result);
-      res.redirect('/');
+      res.redirect("/");
     });
 };
 
@@ -106,86 +121,81 @@ exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
     .removeFromCart(prodId)
-    .then(result => {
-      res.redirect('/cart');
+    .then((result) => {
+      res.redirect("/cart");
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 exports.postOrder = (req, res, next) => {
   req.user
-    .populate('cart.items.productId')
+    .populate("cart.items.productId")
     .execPopulate()
-    .then(user => {
-      const products = user.cart.items.map(i => {
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
         return { quantity: i.quantity, product: { ...i.productId._doc } };
       });
-   
-     
+
       const order = new Order({
-    
         user: {
           email: req.user.email,
-          userId: req.user
-
+          userId: req.user,
         },
         contact: {
-          address : req.body.address,
-          phone: req.body.phone
+          address: req.body.address,
+          phone: req.body.phone,
         },
-          products: products
-
+        products: products,
       });
-      console.log(products)
-      
-      
+      console.log(products);
+      transporter.sendMail({
+        to: "hady.elhossiny1132@gmail.com",
+        from: "SmartStore@node-complete.com",
+        subject: "Orders!",
+        html: "hello",
+      });
+
       return order.save();
     })
-    .then(result => {
+    .then((result) => {
       return req.user.clearCart();
     })
-    
 
- 
     .then(() => {
-      res.redirect('/orders'); 
-      
+      res.redirect("/orders");
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  const email=req.user.email;
-  const admin ='hady.elhossiny1132@gmail.com'
-if (admin == email){
-
-Order.find()
-  .then(orders => {
-    // orders.sort();
-    orders.reverse();
-    res.render('shop/orders', {
-      path: '/orders',
-      pageTitle: 'Your Orders',
-      orders: orders
-    });
-  })
-  .catch(err => console.log(err));
-
-  
-
-}else{
-
-
-  Order.find({ 'user.userId': req.user._id })
-    .then(orders => {
-      // orders.sort();
-      orders.reverse();
-      res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders',
-        orders: orders
-      });
-    })
-    .catch(err => console.log(err));
-}};
-
+  const email = req.user.email;
+  const admin = "hady.elhossiny1132@gmail.com";
+  if (admin == email) {
+    Order.find()
+      .then((orders) => {
+        // orders.sort();
+        orders.reverse();
+        res.render("shop/orders", {
+          path: "/orders",
+          pageTitle: "Your Orders",
+          orders: orders,
+        });
+      })
+      .catch((err) => console.log(err));
+  } else {
+    Order.find({ "user.userId": req.user._id })
+      .then((orders) => {
+        // orders.sort();
+        orders.reverse();
+        res.render("shop/orders", {
+          path: "/orders",
+          pageTitle: "Your Orders",
+          orders: orders,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+};
+function escapeRegex(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
